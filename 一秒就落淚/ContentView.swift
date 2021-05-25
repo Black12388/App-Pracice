51ff6d30d82898e06da2a15031f1bf7e0483869f
Black <black19900916@gmail.com>
2021-05-25 20:58:22 +0800

    Initial Commit

---
+++ 一秒就落淚/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  一秒就落淚
+//
+//  Created by Apple on 2021/5/25.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
